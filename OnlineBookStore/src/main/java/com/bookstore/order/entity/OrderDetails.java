c2981918f79608dd6a860f1186654fc8455a5360
anjali <anjalisharmarnq@gmail.com>
2020-07-17 10:54:33 +0530

    commit

--- OnlineBookStore/src/main/java/com/bookstore/order/entity/OrderDetails.java
+++ OnlineBookStore/src/main/java/com/bookstore/order/entity/OrderDetails.java
@@ -2,20 +2,15 @@ package com.bookstore.order.entity;
 
 
 
-import java.sql.Date;
-import java.util.List;
 
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
